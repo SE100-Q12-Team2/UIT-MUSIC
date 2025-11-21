ad454ff06452f6b59a974db6bc418b5491829651
Nguyễn Tuấn Lộc ( aka Johan) <nguyenloc24022005@gmail.com>
2025-11-21 15:06:09 +0700

    Fix: Remove auth check for health check route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { Auth } from 'src/shared/decorators/auth.decorator'
+import { AuthType } from 'src/shared/constants/auth.constant'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Auth([AuthType.None])
   getHello(): string {
     return this.appService.getHello()
   }
 
   @Get('health')
+  @Auth([AuthType.None])
   healthCheck() {
     return {
       status: 'ok',
